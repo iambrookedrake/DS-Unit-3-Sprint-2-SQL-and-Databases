31323232b122c15a6c0835d36ea74d391298b635
Brooke Drake <iambrookedrake@gmail.com>
2020-07-28 15:59:19 -0600

    Assn2, final

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import psycopg2
+from psycopg2.extras import execute_values
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,61 +18,51 @@ DB_PASS = 'iIIvP8NBWsCVtAY8gdkgiVr_nz5SYwjl'
 DB_HOST = 'hansken.db.elephantsql.com'
 '''
 
-### Connect to ElephantSQL-hosted PostgreSQL
-connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+# Connect to ElephantSQL-hosted PostgreSQL
+connection = psycopg2.connect(
+    dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 print("CONNECTION:", connection)
 
-### A "cursor", a structure to iterate over db records to perform queries
+# A "cursor", a structure to iterate over db records to perform queries
 cursor = connection.cursor()
 print("CURSOR:", cursor)
 
 connection.commit()
-### An example query
-#cursor.execute('SELECT * from titanic')
-
-#results = cursor.fetchall()
-#print(results)
-#connection.commit()
-#########
-
-#df = pd.read_csv("titanic.csv")
-#df.to_records()
-
 
 ##Create Character Table in PostGRES ####
 create_titanic_table_query = '''
-DROP TABLE IF EXISTS titanic_1;
-DROP TABLE IF EXISTS titanic_list;
 DROP TABLE IF EXISTS titanic_table;
 CREATE TABLE titanic_table (
-   Survived                BIT  NOT NULL PRIMARY KEY,
+   id                      SERIAL PRIMARY KEY,
+   Survived                BOOLEAN  NOT NULL,
    Pclass                  INTEGER  NOT NULL,
    Name                    VARCHAR(81) NOT NULL,
    Sex                     VARCHAR(20) NOT NULL,
    Age                     NUMERIC(3) NOT NULL,
    SiblingsSpouses_Aboard  INTEGER  NOT NULL,
    ParentsChildren_Aboard  INTEGER  NOT NULL,
-   Fare                    NUMERIC(8,2) NOT NULL
+   Fare                    NUMERIC(8,4) NOT NULL
 )
 '''
 
 cursor.execute(create_titanic_table_query)
 connection.commit()
 
-cursor.execute('SELECT * from titanic')
-passengers = cursor.fetchall()
-## insert data in postgres#####
+FILEPATH = 'titanic.csv'
+df = pd.read_csv(FILEPATH)
+print(df.columns.tolist())
+# do this before converting to native types, because this actually converts to np.bool
+df["Survived"] = df["Survived"].values.astype(bool)
+# converts numpy dtypes to native python dtypes (avoids psycopg2.ProgrammingError: can't adapt type 'numpy.int64')
+df = df.astype("object")
 
-for passenger in passengers:
-    insert_query = f'''INSERT INTO titanic_table
-		(Survived, Pclass, Name, Sex, Age, SiblingsSpouses_Aboard, ParentsChildren_Aboard, Fare) VALUES
-    	{passenger}
-    '''
-    cursor.execute(insert_query)
+titanic_list = list(df.to_records(index=False))
 
-results2 = cursor.fetchall()
-print("results2: ", results2)
-#print(insert_query)
-connection.commit()
+insert_query = f"""INSERT INTO titanic_table (survived, pclass, name, sex, age, SiblingsSpouses_Aboard, ParentsChildren_Aboard, fare) VALUES %s"""
+execute_values(cursor, str(insert_query), titanic_list)
 
+
+# CLEAN UP
+connection.commit() # actually save the records / run the transaction to insert rows
+print('Titanic Data successfully saved to Postgres!')
